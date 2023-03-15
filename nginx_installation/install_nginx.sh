b62c77db79dcdda3feb19705a59ca23e2c3f91e9
swapnilz30 <swapnilzarbade@gmail.com>
2023-03-15 15:13:22 +0530

    modified:   install_nginx.sh

--- nginx_installation/install_nginx.sh
+++ nginx_installation/install_nginx.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-check_nginx_installed(){
-     sudo apt list --installed | grep nginx
-	 if [ $? -ne 0 ]; then
-	     return
-	 fi
-	 echo "Nginx is already installed"
-	 exit 1
-}
-
 install_nginx(){
-    sudo apt install nginx -y
-	if [ $? -ne 0 ]; then
-	    echo "Failed to install nginx"
-		exit 1
-	fi
-	echo "Nginx successfully installed"
+    sudo apt install nginx -y || echo "Installation failed && exit 1"
+    echo "Nginx successfully installed"
 }
 
 #####Main#####
-check_nginx_installed
 install_nginx
